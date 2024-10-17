cc9853bee371215827427265ffb4e41ec3a739fc
PrinceAFelix <princefelix23@icloud.com>
2024-10-17 09:49:02 -0400

    GET request tested locally

--- src/functions/RESTFunction.js
+++ src/functions/RESTFunction.js
@@ -1,6 +1,8 @@
-const { app } = require('@azure/functions');
-const { CosmosClient } = require("@azure/cosmos");
+import dotenv from 'dotenv';
+import { app } from '@azure/functions';
+import { CosmosClient } from "@azure/cosmos";
 
+dotenv.config();
 
 // Initialzie Cosmos Client
 const client = new CosmosClient(process.env.PRIMARY_CONNECTION_STRING);
@@ -10,13 +12,13 @@ const databaseId = process.env.DATABASE_ID;
 const containerId = process.env.CONTAINER_ID;
 
 app.http('RESTFunction', {
-    methods: ['GET', 'POST'],
+    methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
+
         context.log(`Http function processed request for URL "${request.url}"`);
 
         try {
-            // Query to get all users
             const query = "SELECT c.id, c.name, c.email, c.age FROM c";
 
             // Execute the query to retrieve data from Cosmos DB
@@ -27,22 +29,26 @@ app.http('RESTFunction', {
                 .query(query)
                 .fetchAll();
 
-            // Log the retrieved items
-            context.log("Retrieved items: ", items);
-
-            // Return the result
-            return {
+            // Set response for GET
+            return context.res = {
                 status: 200,
-                body: items
+                body: JSON.stringify(items),
+                headers: {
+                    "Content-Type": "application/json"
+                }
             };
+
         } catch (error) {
             // Log any errors encountered during the query
             context.log("Error retrieving users: ", error.message);
 
             // Return an error response
-            return {
+            return context.res = {
                 status: 500,
-                body: "Error retrieving users: " + error.message
+                body: JSON.stringify({ error: "Error retrieving users: " + error.message }),
+                headers: {
+                    "Content-Type": "application/json" // Set the content type to JSON
+                }
             };
         }
     }
