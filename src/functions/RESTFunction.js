16d91396706deeae613c8000e3506a00806a9d4d
gundami <gundami0096@outlook.com>
2024-10-20 00:34:35 -0400

    Implement DELETE method and complete GET method

--- src/functions/RESTFunction.js
+++ src/functions/RESTFunction.js
@@ -11,11 +11,13 @@ const client = new CosmosClient(process.env.PRIMARY_CONNECTION_STRING);
 const databaseId = process.env.DATABASE_ID;
 const containerId = process.env.CONTAINER_ID;
 
-app.http('RESTFunction', {
-    methods: ['GET'],
-    authLevel: 'function',
-    handler: async (request, context) => {
 
+app.http('RESTFunction1', {
+    methods: ['GET', 'PUT', 'DELETE', 'POST'],
+    authLevel: 'function',
+    route: 'users/{id:int?}',
+    handler: async (request, context) => {
+        const id = request.params.id;
         context.log(`Http function processed request for URL "${request.url}"`);
 
         const { method } = request;
@@ -23,33 +25,97 @@ app.http('RESTFunction', {
         try {
             switch (method) {
                 case 'GET':
-                    const query = "SELECT c.id, c.name, c.email, c.age FROM c";
+                    if (id) {
+                        const query = `SELECT c.id, c.name, c.email, c.age FROM c WHERE c.id = '${id}'`;
 
-                    // Execute the query to retrieve data from Cosmos DB
-                    const { resources: items } = await client
-                        .database(databaseId)
-                        .container(containerId)
-                        .items
-                        .query(query)
-                        .fetchAll();
+                        // Execute the query to retrieve data from Cosmos DB
+                        const { resources: items } = await client
+                            .database(databaseId)
+                            .container(containerId)
+                            .items
+                            .query(query)
+                            .fetchAll();
 
-                    // Set response for GET
-                    return context.res = {
-                        status: 200,
-                        body: JSON.stringify(items),
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
-                    };
+                        // Set response for GET
+                        return context.res = {
+                            status: 200,
+                            body: JSON.stringify(items),
+                            headers: {
+                                "Content-Type": "application/json"
+                            }
+                        };
+                    } else {
+                        const query = `SELECT c.id, c.name, c.email, c.age FROM c`;
+
+                        // Execute the query to retrieve data from Cosmos DB
+                        const { resources: items } = await client
+                            .database(databaseId)
+                            .container(containerId)
+                            .items
+                            .query(query)
+                            .fetchAll();
+
+                        // Set response for GET
+                        return context.res = {
+                            status: 200,
+                            body: JSON.stringify(items),
+                            headers: {
+                                "Content-Type": "application/json"
+                            }
+                        };
+                    }
                 case 'POST':
-                    // Handle POST request (create a new user)
-                    break;
+                // Handle POST request (create a new user)
+
                 case 'PUT':
                     // Handle PUT request (update an existing user)
                     break;
                 case 'DELETE':
-                    // Handle DELETE request (delete an existing user)
-                    break;
+                    // Handle DELETE request (delete an existing user
+
+                    if (id) {
+                        //Check if the id is already in the database
+                        var query = `SELECT * FROM c WHERE c.id = '${id}'`;
+
+                        // Execute the query to retrieve data from Cosmos DB
+                        var { resources: item } = await client
+                            .database(databaseId)
+                            .container(containerId)
+                            .items
+                            .query(query)
+                            .fetchNext();
+                        console.log(item[0]);
+                        // Delete the user with the specified ID
+                        await client
+                            .database(databaseId)
+                            .container(containerId)
+                            .item(item[0].id, item[0].name)
+                            .delete();
+                        return context.res = {
+                            status: 200,
+                            headers: {
+                                "Content-Type": "application/json"
+                            }
+                        };
+
+                    } else {
+                        // Return an error response if the ID is missing
+                        return context.res = {
+                            status: 400,
+                            body: JSON.stringify({ error: "Missing ID" }),
+                            headers: {
+                                "Content-Type": "application/json"
+                            }
+                        };
+                    } query
+                    return context.res = {
+                        status: 200,
+                        body: JSON.stringify(requestData),
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
+                    };
+
                 default:
                     // Handle unsupported methods
                     context.res = {
